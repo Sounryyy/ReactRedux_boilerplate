cac4aa78b5b7f892bc11b9455cc993d1de35ef35
JTS <jtsvig@gmail.com>
2019-07-08 17:02:03 +0300

    Added prettier

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 
-import './css.css'
+import './css.css';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-    };
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <div>
-                <h1> React is working!!!!!!!!!!!</h1>
-                <h1 className='red'>Hello world!</h1><h2>Осталось - Router, Redux, eslint, prettier, babel(+es6, +react)</h2>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1> React is working!!!!!!!!!!!</h1>
+        <h1 className="red">Hello world!</h1>
+        <h2>Осталось - Router, Redux, eslint, prettier, babel(+es6, +react)</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
