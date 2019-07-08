93c77b69963e5c8e1fe37430b7963c7e4a1ba2b2
JTS <jtsvig@gmail.com>
2019-07-08 14:25:05 +0300

    Added babel

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './css.css'
 
-document.getElementById('root').innerHTML = '<h1 class="red">Hello world!</h1>';
+document.getElementById('root').innerHTML = '<h1 class="red">Hello world!</h1><h2>Осталось - React, Router, Redux, eslint, prettier, babel</h2>';
+console.log([1,2,3].map(x => x + 1));
