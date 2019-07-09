6a966f4693427a20c2c90429ccf238152027f607
JTS <jtsvig@gmail.com>
2019-07-09 16:17:54 +0300

    Added translations and react-router-redux react-router-scroll packages

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ class App extends Component {
         <h1> React is working!!!!!!!!!!!</h1>
         <h1>Hello world!</h1>
         <h2>Осталось</h2>
-        <h3>Цель на 09.07.2019 - модульная архитектура, code splitting, шаблон</h3>
+        <h3>
+          Цель на 09.07.2019 - модульная архитектура, code splitting, prod version of webpack шаблон
+        </h3>
       </div>
     );
   }
